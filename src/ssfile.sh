40d2687ebb533b6ab3c22a93d38f266fca28b033
Milad Abooali <m.abooali@hotmail.com>
2023-02-28 12:07:18 +0330

    Base Structure

---
+++ src/ssfile.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# SSFIle v 0.1
+# by Milad Abooali (m.abooali@hotmail.com)
+# developed for Codebxo (codebox.ir)
+# Last Change 2023-02-28
+
+### This server (local)
+server_name="Box-1"
+local_path="/etc/test/*"
+
+### Backup Server (remote)
+IP="SERVERIP"
+password="YOURPASSWORD"
+username="root"
+remote_path="/home/backup/"
+
+### Sync Files
+folder_name=${server_name})_`date +%Y-%M-%d_%H`.avi
+sshpass -p "$password" scp -rC $local_path $username@$IP:$remote_path$folder_name
+
+### Add Log
+echo "Files uploaded from '${local_path}'to '${remote_path}' on remote server: ${server_name} (${IP}) " >> /var/log/ssfile.log
