c272612046275c46400664314b7a8e11aed26609
Milad Abooali <m.abooali@hotmail.com>
2023-02-28 13:03:55 +0330

    Flow

--- src/ssfile.sh
+++ src/ssfile.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# SSFIle v 0.1
+# SSFile v 0.1
 # by Milad Abooali (m.abooali@hotmail.com)
-# developed for Codebxo (codebox.ir)
+# developed for Codebox (codebox.ir)
 # Last Change 2023-02-28
 
 ### This server (local)
@@ -16,8 +16,8 @@ username="root"
 remote_folder="/home/backup"
 
 ### Sync Files
-remote_path="${remote_folder}/${server_name}_"`date +%Y-%M-%d_%H`
-sshpass -p "$password" scp -rC $local_path $username@$IP:$remote_path$remote_path
+remote_path="${remote_folder}/${server_name}_"`date +%Y-%m-%d_%H`
+sshpass -p "$password" scp -rC $local_path $username@$IP:$remote_path
 
 ### Add Log
-echo "Files uploaded from '${local_path}'to '${remote_path}' on remote server: ${server_name} (${IP}) " >> /var/log/ssfile.log
+echo `date +%Y-%m-%d_%H:%M:%S`" Files uploaded from '${local_path}'to '${remote_path}' on remote server: ${server_name} (${IP}) " >> /var/log/ssfile.log
